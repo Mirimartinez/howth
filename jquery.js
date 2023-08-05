08d266538f63b1c358373df20f3ca86584693cca
Miriana Martinez <mirianamartinezok@gmail.com>
2023-08-05 17:03:45 -0300

    first commit

---
+++ jquery.js
@@ -0,0 +1,8 @@
+// $(document).ready(function(){
+//     $(document).mousemove(function(e){
+//       $('img').css('left',e.pageX+"px");
+//       $('img').css('top',e.pageY+"px");
+//     });
+//   });
+
+  
